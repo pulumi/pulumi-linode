47486729cd8346c661eef42164797a9cc40cd892
Marques Johansson <marques@linode.com>
2019-02-06 22:44:41 -0500

    simplify tests

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -1,96 +1,45 @@
-// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package examples
 
 import (
-	"io/ioutil"
-	"net/http"
 	"os"
+	"path"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/testing/integration"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestExamples(t *testing.T) {
-	// Ensure we have any required configuration points
-	token := os.Getenv("LINODE_TOKEN")
-	if token == "" {
-		t.Skipf("Skipping test due to missing LINODE_TOKEN environment variable")
-	}
+var base = integration.ProgramTestOptions{
+	ExpectRefreshChanges: true,
+	// Note: no Config! This package should be usable without any config.
+}
+
+func TestWebserver(t *testing.T) {
 	cwd, err := os.Getwd()
-	if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
-		return
+	if !assert.NoError(t, err) {
+		t.FailNow()
 	}
 
-	// base options shared amongst all tests.
-	base := integration.ProgramTestOptions{
-		Config: map[string]string{
-			// Configuration map
-		},
-		Tracing: "https://tracing.pulumi-engineering.com/collector/api/v1/spans",
-	}
-	baseJS := base.With(integration.ProgramTestOptions{
+	opts := base.With(integration.ProgramTestOptions{
 		Dependencies: []string{
-			// JavaScript dependencies
+			"@pulumi/linode",
 		},
+		Dir: path.Join(cwd, "webserver"),
 	})
-
-	examples := []integration.ProgramTestOptions{
-		// List each test
-		// baseJS.With(integration.ProgramTestOptions{
-		// 	Dir: path.Join(cwd, "api"),
-		// 	ExtraRuntimeValidation: validateAPITest(func(body string) {
-		// 		assert.Equal(t, "Hello, world!", body)
-		// 	}),
-		// 	EditDirs: []integration.EditDir{{
-		// 		Dir:      "./api/step2",
-		// 		Additive: true,
-		// 		ExtraRuntimeValidation: validateAPITest(func(body string) {
-		// 			assert.Equal(t, "<h1>Hello world!</h1>", body)
-		// 		}),
-		// 	}},
-		// 	ExpectRefreshChanges: true,
-		// }),
-	}
-
-	if !testing.Short() {
-		// Append any longer running tests
-	}
-
-	for _, ex := range examples {
-		example := ex
-		t.Run(example.Dir, func(t *testing.T) {
-			integration.ProgramTest(t, &example)
-		})
-	}
-}
-
-func createEditDir(dir string) integration.EditDir {
-	return integration.EditDir{Dir: dir, ExtraRuntimeValidation: nil}
-}
-
-func validateAPITest(isValid func(body string)) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-		var resp *http.Response
-		var err error
-		url := stack.Outputs["url"].(string)
-		// Retry a couple times on 5xx
-		for i := 0; i < 2; i++ {
-			resp, err = http.Get(url + "/b")
-			if !assert.NoError(t, err) {
-				return
-			}
-			if resp.StatusCode < 500 {
-				break
-			}
-			time.Sleep(10 * time.Second)
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		assert.NoError(t, err)
-		isValid(string(body))
-	}
+	integration.ProgramTest(t, &opts)
 }
